d955d11e61cba3d94b82152b1bac85ec29a239ab
Alaric Snell-Pym <alaric@dotmesh.com>
2019-07-02 14:39:52 +0100

    NFC: Make the quick setup script set up the whole crontab from scratch, it's simpler

--- deploy-cleanup-script-to-runners.sh
+++ deploy-cleanup-script-to-runners.sh
@@ -3,5 +3,12 @@ do
     echo "Deploying cleanup-old-test-resources.sh and cronjob to $i..."
     SSH_TARGET=gitlab-runner@$i
     scp cleanup-old-test-resources.sh $SSH_TARGET:cleanup-old-test-resources.sh
-    ssh $SSH_TARGET 'if sudo crontab -l | grep cleanup-old-test-resources.sh; then echo already done; else (sudo crontab -l ; echo "*/5 * * * * (/home/gitlab-runner/cleanup-old-test-resources.sh 2>&1 | ts) >> /home/gitlab-runner/cleanup-old-test-resources.log") | sudo crontab -; fi'
+    ssh $SSH_TARGET 'sudo crontab -' <<EOF
+# Please don't hand-edit this crontab, use citools/deploy-cleanup-script-to-runners.sh to keep
+# all the runners in sync (and if that annoys you, then install chef/puppet/etc to manage them properly)
+
+@daily rm -f /etc/zfs/zpool.cache && reboot -f
+@reboot rm -rf /dotmesh-test-pools
+* * * * * (/home/gitlab-runner/cleanup-old-test-resources.sh 2>&1 | ts) >> /home/gitlab-runner/cleanup-old-test-resources.log
+EOF
 done
